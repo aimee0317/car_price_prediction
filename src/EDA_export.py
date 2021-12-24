ec79175314914f975994c05035241131dfa89717
Amelia Tang <amelia9@student.ubc.ca>
2021-12-24 03:12:20 -0800

    Update EDA and EDA script and update the model

--- src/EDA_export.py
+++ src/EDA_export.py
@@ -1,11 +1,12 @@
 """
 EDA plots
-Usage: EDA_export.py --plot1path=<plot1path> --plot2path=<plot2path> --plot3path=<plot3path>
+Usage: EDA_export.py --plot1path=<plot1path> --plot2path=<plot2path> --plot3path=<plot3path> --plot4path=<plot4path>
 
 Options:
---plot1path=<plot1path>              file path of the target_distribution_plot
---plot2path=<plot2path>              file path of the price_by_brand
---plot3_path=<plot3path>             file path of corr_plot
+--plot1path=<plot1path>              file path of target_distribution_plot
+--plot2path=<plot2path>              file path of price_by_brand
+--plot3path=<plot3path>              file path of corr_plot
+--plot4path=<plot4path>              file path of price_year
 """
 
 
@@ -24,11 +25,12 @@ X_train['year'] = X_train['year'].dt.year
 y_train = pd.read_csv("data/raw/y_train.csv")
 train_df = X_train.join(y_train.set_index('carID'), on = "carID")
 
-def main(plot1path, plot2path, plot3path):
+def main(plot1path, plot2path, plot3path, plot4path):
   
   target_distribution_plot(plot1path)
   price_by_brand(plot2path)
   corr_plot(plot3path)
+  price_year(plot4path)
 
     
 def target_distribution_plot(plot1path):
@@ -100,7 +102,27 @@ def corr_plot(plot3path):
      
   plot3.save(plot3path, scale_factor=4) 
   
+def price_year(plot4path):
+    """
+    Generate price distribution plot by year for each brand 
+  
+    Parameters:
+    --------
+    plot4path : str
+      path to save the plot
+    
+    Returns:
+    --------
+    save the plot to a specified directory
+    """
+    plot4 = alt.Chart(train_df).mark_boxplot().encode(
+     y='price',
+     x=alt.X('year', scale=alt.Scale(zero=False)),
+     color=alt.Color('year', legend=None)).facet('brand', columns=1, title = 'Price Distribution by Year for Each Brand')
+    
+    plot4.save(plot4path, scale_factor=4)
+  
   
 if __name__ == "__main__":
-    main(opt["--plot1path"], opt["--plot2path"], opt["--plot3path"])
+    main(opt["--plot1path"], opt["--plot2path"], opt["--plot3path"], opt["--plot4path"])
   
