19b6d71da1b17d2f0c98c22c0c6f376555fe479c
Amelia Tang <amelia9@student.ubc.ca>
2022-02-22 21:15:05 -0800

    Upload deployment files and model saved as pickle

---
+++ deployment/deployment.py
@@ -0,0 +1,42 @@
+# Author: Amelia Tang
+
+from flask import Flask,request, url_for, redirect, render_template, jsonify
+import pandas as pd
+import pickle
+import numpy as np
+from sklearn.compose import ColumnTransformer, make_column_transformer
+from catboost import CatBoostRegressor
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler
+
+# Initalize the Flask app
+app = Flask(__name__, template_folder='templates')
+
+# Loads pre-trained model
+model = pickle.load(open('final_model.pkl', 'rb'))
+cols = ["brand", "model", "year", "transmission", "mileage", "fuelType", "tax", "mpg", "engineSize"]
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [x for x in request.form.values()]
+    final = np.array(int_features)
+    data_unseen = pd.DataFrame([final], columns = cols)
+    prediction = model.predict(data_unseen)
+    prediction = int(prediction)
+    return render_template('home.html',pred='Expected price will be {}'.format(prediction))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    data_unseen = pd.DataFrame([data])
+    prediction = model.predict(data_unseen)
+    output = prediction.Label[0]
+    return jsonify(output)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
