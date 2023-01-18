f48ccdf1eaf1181e3f4095f6596f17d16a224efd
Amelia Tang <aimee.tang0317@outlook.com>
2023-01-17 21:32:27 -0800

    Complete model_selection script

--- src/model_selection.py
+++ src/model_selection.py
@@ -9,20 +9,25 @@ Options:
 """
 
 import os
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.compose import ColumnTransformer, make_column_transformer
+import altair as alt
+from sklearn.compose import make_column_transformer
 from sklearn.dummy import DummyRegressor
 from sklearn.ensemble import RandomForestRegressor
-from catboost import CatBoostRegressor
 from sklearn.linear_model import Ridge, Lasso
 from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.model_selection import (
- cross_val_score,
- cross_validate
+    cross_val_score,
+    cross_validate,
+    train_test_split,
+    GridSearchCV,
+    RandomizedSearchCV
 )
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler
+from xgboost import XGBRegressor
 
 from docopt import docopt
 
@@ -34,13 +39,8 @@ def main(csv_path):
     X_train = pd.read_csv("data/raw/X_train.csv", parse_dates=['year'])
     X_train['year'] = X_train['year'].dt.year
     y_train = pd.read_csv("data/raw/y_train.csv")
-    train_df = X_train.join(y_train.set_index('carID'), on = "carID")
-    
-    # read test data 
-    X_test = pd.read_csv("data/raw/X_test.csv", parse_dates=['year'])
-    X_test['year'] = X_test['year'].dt.year
-    y_test = pd.read_csv("data/raw/y_test.csv")
-  
+    train_df = X_train.join(y_train.set_index('carID'), on="carID")
+
     # separate X and y
     X_train, y_train = train_df.drop(columns="price"), train_df["price"]
 
@@ -69,27 +69,28 @@ def preprocess(train_df):
     ----------
         sklearn ColumnTransformer
     """
-    # split columns into different categories 
+    # split columns into different categories
     drop_features = ["carID"]
     categorical_features = ["brand", "model", "transmission", "fuelType"]
     ordinal_features = ["year"]
     target = "price"
     numeric_features = list(
-      set(train_df.columns)
-      - set(drop_features)
-      - set(categorical_features)
-      - set(ordinal_features)
-      - set([target])
+        set(train_df.columns)
+        - set(drop_features)
+        - set(categorical_features)
+        - set(ordinal_features)
+        - set([target])
     )
-    
+
     preprocessor = make_column_transformer(
         (StandardScaler(), numeric_features),
         (OneHotEncoder(handle_unknown="ignore", dtype="int"), categorical_features),
-        (OrdinalEncoder(handle_unknown="use_encoded_value", unknown_value=np.nan), ordinal_features),
+        (OrdinalEncoder(handle_unknown="use_encoded_value",
+         unknown_value=np.nan), ordinal_features),
         ("drop", drop_features))
 
     return preprocessor
-  
+
 
 def mean_std_cross_val_scores(model, X_train, y_train, **kwargs):
     """
@@ -115,7 +116,8 @@ def mean_std_cross_val_scores(model, X_train, y_train, **kwargs):
     out_col = []
 
     for i in range(len(mean_scores)):
-        out_col.append((f"%0.3f (+/- %0.3f)" % (mean_scores[i], std_scores[i])))
+        out_col.append((f"%0.3f (+/- %0.3f)" %
+                       (mean_scores[i], std_scores[i])))
 
     return pd.Series(data=out_col, index=mean_scores.index)
 
@@ -138,16 +140,16 @@ def generate_scores(preprocessor, X_train, y_train):
         pandas data frame with mean cross-validation scores
     """
     scoring_metrics = {
-    "neg RMSE": "neg_root_mean_squared_error",
-    "r2": "r2"}
+        "neg RMSE": "neg_root_mean_squared_error",
+        "r2": "r2"}
 
     # base line
-    results ={}
+    results = {}
     dummy = DummyRegressor()
     results["Dummy"] = mean_std_cross_val_scores(
-      dummy, X_train, y_train, return_train_score=True, scoring=scoring_metrics
+        dummy, X_train, y_train, return_train_score=True, scoring=scoring_metrics
     )
-    
+
     # model pipelines
     pipe_lr = make_pipeline(
         preprocessor, Ridge(random_state=123)
@@ -161,26 +163,26 @@ def generate_scores(preprocessor, X_train, y_train):
         preprocessor, RandomForestRegressor(random_state=123)
     )
 
-    pipe_catboost = make_pipeline(
-        preprocessor, CatBoostRegressor(random_state=123, verbose = 0)
+    pipe_xgboost = make_pipeline(
+        preprocessor, XGBRegressor(random_state=123)
     )
-    
-    # models 
+
+    # models
     models = {
         "ridge": pipe_lr,
         "lasso": pipe_ls,
         "random forest": pipe_rf,
-        "CatBoost": pipe_catboost
+        "XGBoost": pipe_xgboost
     }
 
     # generate results
     for (name, model) in models.items():
-      results[name] = mean_std_cross_val_scores(
-        model, X_train, y_train, return_train_score=True, scoring=scoring_metrics
-      )
+        results[name] = mean_std_cross_val_scores(
+            model, X_train, y_train, return_train_score=True, scoring=scoring_metrics
+        )
 
     return pd.DataFrame(results)
-  
+
 
 if __name__ == "__main__":
     main(opt["--csv_path"])
