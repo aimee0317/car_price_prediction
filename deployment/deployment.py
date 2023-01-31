7f4dd6acec2c2277bc1949751f928b787f66253a
Amelia Tang <aimee.tang0317@outlook.com>
2023-01-31 13:04:05 -0800

    Updated deployment script

--- deployment/deployment.py
+++ deployment/deployment.py
@@ -1,35 +1,36 @@
 # Author: Amelia Tang
 
-from flask import Flask,request, url_for, redirect, render_template, jsonify
+from flask import Flask, request, url_for, redirect, render_template, jsonify
 import pandas as pd
-import pickle
 import numpy as np
-from sklearn.compose import ColumnTransformer, make_column_transformer
-from catboost import CatBoostRegressor
-from sklearn.pipeline import Pipeline, make_pipeline
-from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler
+import pickle
+
 
 # Initalize the Flask app
 app = Flask(__name__, template_folder='templates')
 
 # Loads pre-trained model
-model = pickle.load(open('final_model.pkl', 'rb'))
-cols = ["brand", "model", "year", "transmission", "mileage", "fuelType", "tax", "mpg", "engineSize"]
+model = pickle.load(open('deployment/final_model.pickle', 'rb'))
+cols = ["brand", "model", "year", "transmission",
+        "mileage", "fuelType", "tax", "mpg", "engineSize"]
+
 
 @app.route('/')
 def home():
     return render_template("home.html")
 
-@app.route('/predict',methods=['POST'])
+
+@app.route('/predict', methods=['POST'])
 def predict():
     int_features = [x for x in request.form.values()]
     final = np.array(int_features)
-    data_unseen = pd.DataFrame([final], columns = cols)
+    data_unseen = pd.DataFrame([final], columns=cols)
     prediction = model.predict(data_unseen)
     prediction = int(prediction)
-    return render_template('home.html',pred='Predicted used car price is {}'.format(prediction))
+    return render_template('home.html', pred='Predicted used car price is {}'.format(prediction))
 
-@app.route('/predict_api',methods=['POST'])
+
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
     data = request.get_json(force=True)
     data_unseen = pd.DataFrame([data])
